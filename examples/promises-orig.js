664dcbd1a9b8f1ccfd1ed2c6349419788dc21fdb
thomasabishop <tactonbishop@gmail.com>
2021-03-05 21:53:41 +0000

    Update examples of Promises and async await

---
+++ examples/promises-orig.js
@@ -0,0 +1,71 @@
+const fetch = require('node-fetch');
+//const axios = require('axios').default;
+
+function githubUserLocation(username) {
+  fetch(`https://api.github.com/users/${username}`)
+    .then((response) => {
+      if (response.ok) {
+        console.log(response);
+        return response;
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(`${username} hails from ${data.location}`))
+    .catch((err) => console.error(err));
+}
+//githubUserLocation("thomasabishop");
+
+function githubUserLocationTwo(username) {
+  fetch(`https://api.github.com/users/${username}`)
+    .then(function (response) {
+      console.log(response.headers);
+      response.json().then(function (data) {
+        console.log(data.location);
+      });
+    })
+    .catch((err) => console.error(err));
+}
+
+githubUserLocationTwo('thomasabishop');
+
+async function githubUserLocationAsync(username) {
+  try {
+    let response = await fetch(`https://api.github.com/users/${username}`);
+    let data = await response.json();
+    if (!response.ok) {
+      throw new Error(`Error. Status: ${response.status}`);
+    } else {
+      let location = data.location;
+      console.log(location);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//githubUserLocationAsync("thomasabishop").catch((error) => console.error(error));
+
+/* 
+ githubUserLocationAsync("thomasabishop").then(function (data) {
+  console.log(data.location);
+});  */
+
+/* function githubUserLocationAxios(username) {
+  axios
+    .get(`https://api.github.com/users/${username}`)
+    .then((response) => console.log(response.data))
+    .catch((error) => console.error(error));
+} */
+
+// async function githubUserLocationAxios(username) {
+//   try {
+//     const response = await axios.get(
+//       `https://api.github.com/users/${username}`
+//     );
+//     console.log(response.data.location);
+//   } catch = (error) => {
+//     console.error(error);
+//   }
+// }
+
+// githubUserLocationAxios("thomasabishop");
--- examples/promises.js
+++ examples/promises.js
@@ -1,70 +1,72 @@
-const fetch = require("node-fetch");
-const axios = require("axios").default;
+const fetch = require('node-fetch');
 
-function githubUserLocation(username) {
-  fetch(`https://api.github.com/users/${username}`)
-    .then((response) => {
-      if (response.ok) {
-        console.log(response);
-        return response;
-      }
-    })
-    .then((response) => response.json())
-    .then((data) => console.log(`${username} hails from ${data.location}`))
-    .catch((err) => console.error(err));
-}
-//githubUserLocation("thomasabishop");
+/* Manual Promise construction, without Fetch */
 
-function githubUserLocationTwo(username) {
-  fetch(`https://api.github.com/users/${username}`)
-    .then(function (response) {
-      response.json().then(function (data) {
-        console.log(data.location);
-      });
-    })
-    .catch((err) => console.error(err));
-}
+// function addTwo(n) {
+//   return n + 2;
+// }
 
-//githubUserLocationTwo("thomasabishop");
+// let promise = new Promise(function (resolve, reject) {
+//   setTimeout(() => resolve(addTwo(2)), 2000);
+// });
 
-async function githubUserLocationAsync(username) {
-  try {
-    let response = await fetch(`https://api.github.com/users/${username}`);
+// promise.then((value) => {
+//   console.log('resolved with ' + value);
+// });
+
+// let anotherPromise = new Promise(function (resolve, reject) {
+//   resolve(addTwo(null));
+//   reject('there has been a problem');
+// });
+
+// anotherPromise.then((value) => console.log('resolved with ' + value));
+async function getUserLocation() {
+  let response = await fetch('https://api.github.com/users/thomasabishop');
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
     let data = await response.json();
-    if (!response.ok) {
-      throw new Error(`Error. Status: ${response.status}`);
-    } else {
-      let location = data.location;
-      console.log(location);
-    }
+    console.log(data.location);
+  }
+}
+
+getUserLocation().catch(function (error) {
+  console.log(error);
+});
+
+async function queryResponse() {
+  try {
+    let response = await fetch('https://api.github.com/users/thomasabishop');
+    let data = await response.json();
+    let location = data.location;
+    console.log(location);
   } catch (error) {
     console.log(error);
   }
 }
+queryResponse();
 
-//githubUserLocationAsync("thomasabishop").catch((error) => console.error(error));
-
-/* 
- githubUserLocationAsync("thomasabishop").then(function (data) {
-  console.log(data.location);
-});  */
-
-/* function githubUserLocationAxios(username) {
-  axios
-    .get(`https://api.github.com/users/${username}`)
-    .then((response) => console.log(response.data))
-    .catch((error) => console.error(error));
-} */
-
-async function githubUserLocationAxios(username) {
-  try {
-    const response = await axios.get(
-      `https://api.github.com/users/${username}`
-    );
-    console.log(response.data.location);
-  } catch = (error) => {
-    console.error(error);
+async function abstractRetrieval(resource, interfaceMethod) {
+  let response = await fetch(resource);
+  let content;
+  if (!response.ok) {
+    throw new Error(`ERROR! Status: ${reponse.status}`);
+  } else {
+    if (interfaceMethod === 'JSON') {
+      content = await response.json();
+    } else if (interfaceMethod === 'text') {
+      content = await response.text();
+    }
+    return content;
   }
 }
 
-githubUserLocationAxios("thomasabishop");
+async function largeData() {
+  const src = 'https://api.github.com/users/thomasabishop';
+  let username = abstractRetrieval(src, 'JSON');
+  let location = abstractRetrieval(src, 'JSON');
+  let values = await Promise.all([username, location]);
+  console.log(values);
+}
+
+largeData().catch((error) => console.log(error));
